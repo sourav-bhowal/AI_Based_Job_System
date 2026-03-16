416075b26c456a97a8d199fe07a96df3dec573d8
Sourav Bhowal <souravbhowal15@gmail.com>
2026-03-16 10:01:32 +0530

    update .gitignore and model metrics; add new model files and update training timestamps

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-from time import timezone
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
